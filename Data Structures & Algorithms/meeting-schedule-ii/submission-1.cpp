da2efcf120390899eef31550785fe02473526f46
Shan-Meshram <meshramshan999@gmail.com>
2026-07-01 09:05:20 +0530

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        vector<int> s;
+        vector<int> e;
+        for(auto it : intervals ){
+            s.push_back(it.start);
+            e.push_back(it.end);
+        }
+        sort(s.begin(),s.end());
+        sort(e.begin(),e.end());
+        int n = s.size();
+
+        int i=0 ;
+        int j=0;
+        int ans=0;
+        int cnt=0;
+        while(i<n && j<n){
+            if(s[i]<e[j]){
+                cnt++;
+                ans=max(ans,cnt);
+                i++;
+            }
+            else{
+                cnt--;
+                j++;
+            }
+        }
+    return ans;
+    }
+};
